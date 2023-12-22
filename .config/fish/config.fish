d86675b67c32df22a70075cfa4157492051c3318
Ashrith Mekala <ashrith@me.com>
2023-12-22 14:08:35 -0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,11 +6,21 @@ if [ -f "$HOME/.config/fish/functions.fish" ]
 	source "$HOME/.config/fish/functions.fish"
 end
 
-eval (/opt/homebrew/bin/brew shellenv)
+if [ -f "/opt/homebrew/bin/brew" ]
+	eval (/opt/homebrew/bin/brew shellenv)
+end
 
-starship init fish | source
+if [ -d "$HOME/.cargo" ]
+	export PATH="$HOME/.cargo/bin:$PATH"
+end
 
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
-pyenv init - | source
-pyenv virtualenv-init - | source
+if type -q starship
+	starship init fish | source
+end
+
+if type -q pyenv
+	pyenv init - | source
+	pyenv virtualenv-init - | source
+end
